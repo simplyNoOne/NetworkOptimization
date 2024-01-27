f0a4bcf3b4f636d33437833dae0b81b9f04a0b92
Ania000 <annasidor00@gmail.com>
2024-01-27 23:42:44 +0100

    works

--- AGChallenge/Optimizer.cpp
+++ AGChallenge/Optimizer.cpp
@@ -35,7 +35,7 @@ void COptimizer::vInitialize()
 void COptimizer::vRunIteration()
 {
 	iGenerations++;
-	if (iGenerations % I_POP_INCR == 0 && (iCurrentPopSize + I_POP_STEP) <= I_POP_SIZE) {
+	if (iGenerations % I_POP_INCR_INTERVAL == 0 && (iCurrentPopSize + I_POP_STEP) <= I_POP_SIZE) {
 		iCurrentPopSize += I_POP_STEP;
 	}
 	pcPopulation->vEvalSortAll();
@@ -46,6 +46,10 @@ void COptimizer::vRunIteration()
 	if (iGenerations % I_HELPER_POP_MIG == 0) {
 		pcPopulation->vGenBestFromHelper();
 		pcPopulation->vEvalSortIndividuals();
+	}
+	if (iGenerations % I_KILL_WAIT == 0) {
+		pcPopulation->vExterminateClones();
+		pcPopulation->vEvalSortAll();
 	}
 	
 	double dIterFitness = pcPopulation->dGetBestValue();
@@ -60,32 +64,33 @@ void COptimizer::vRunIteration()
 	else{
 		iStagnation++;
 		if (iStagnation % I_WAIT == 0) {
-			if(dGenePenalty < D_MAX_GENE_PEN)
-				dGenePenalty += D_GENE_PUSH;
-			if(dParentPenalty < D_MAX_PARENT_PEN)
-				dParentPenalty += D_PARENT_PUSH;
-			if (dCrossPenalty < D_MAX_CROSS_PEN)
-				dCrossPenalty += D_CROSS_PUSH;
+			vGetNewRandParams();
+			if (iStagnation % I_KILL_STAG_WAIT == 0) {
+				pcPopulation->vExterminateClones();
+				pcPopulation->vEvalSortAll();
+			}
 		}
 	}
 	
 	//cout << dBestFitness << endl;
+	//bsack here
 	pcPopulation->vCrossMutate();
 
-	if (iStagnation % I_WAIT == 0) {
+	if (iStagnation != 0 && iStagnation % I_WAIT == 0) {
+		pcPopulation->vEvalSortIndividuals();
 		if (iStagnation % I_MIG_WAIT == 0 ) {
 			pcPopulation->vExchangeBestGenes();
 		}
-		if (iStagnation % I_HELP_WAIT == 0) {
-			pcPopulation->vGenBestFromHelper();
+		if (iStagnation % ( I_CHAOS_WAIT) == 0) {
+			pcPopulation->vUnleashChaos();
 		}
 	}
 	
+
 	if (iPrevPopSize != iCurrentPopSize) {
 		iSubGrpSize = iCurrentPopSize / I_PARENTS_SUBGRPS;
 		iPrevPopSize += I_POP_STEP;
 	}
-		
 	
 }
 
@@ -93,6 +98,29 @@ void COptimizer::vRunIteration()
 void COptimizer::vRunUntil(const CExecutor* cExecutor)
 {
 	cExecutor->vRun(this);
+}
+
+void COptimizer::vGetNewRandParams()
+{
+	long seed = MyMath::lRand(899'999'999) + 100'000'000;
+	std::default_random_engine generator(seed);
+	std::normal_distribution<double> distribution1(D_PARENT_PEN_MEAN, D_PARENT_PEN_DEV);	
+	dParentPenalty = distribution1(generator);
+	
+	if(MyMath::dRand() > 0.5) {
+		dParentPenalty *= -1;
+	}
+
+	std::normal_distribution<double> distribution2(D_CROSS_PEN_MEAN, D_CROSS_PEN_DEV);
+	dCrossPenalty = distribution2(generator);
+
+	if (MyMath::dRand() > 0.5) {
+		dCrossPenalty *= -1;
+	}
+
+	std::normal_distribution<double> distribution3(D_GENE_PEN_MEAN, D_GENE_PEN_DEV);
+	dGenePenalty = distribution3(generator);
+
 }
 
 
--- AGChallenge/Population.cpp
+++ AGChallenge/Population.cpp
@@ -8,44 +8,45 @@
 
 
 
-
 CSubPopulation::~CSubPopulation()
 {
 	for (auto g : *vpcIndividuals)
 		delete g;
 	delete vpcIndividuals;
-	for (auto e : vpcEvaluators)
-		delete e;
 	delete cEv;
+	if (pcDaBest)
+		delete pcDaBest;
 }
 
 void CSubPopulation::vEvalSortIndividuals()
 {
 #pragma omp parallel for
-	for (int i = 0; i < cOpt->iPrevPopSize; ++i) {
-		(*vpcIndividuals)[i]->vEvaluateFitness(vpcEvaluators[i]);
-	}
+		for (int i = 0; i < cOpt->iPrevPopSize; ++i) {
+			(*vpcIndividuals)[i]->vEvaluateFitness((*pvpcEvaluators)[i]);
+		}
 
 	std::sort(vpcIndividuals->begin(), vpcIndividuals->end(), [](CIndividual* a, CIndividual* b) { return a->dGetFitness() > b->dGetFitness(); });
+
+	if (!pcDaBest) {
+		pcDaBest = new CIndividual( vpcIndividuals->at(0));
+	}
+	else {
+		if (vpcIndividuals->at(0)->dGetFitness() > pcDaBest->dGetFitness()) {
+			delete pcDaBest;
+			pcDaBest = new CIndividual(vpcIndividuals->at(0));
+		}
+			
+	}
 }
 
 void CSubPopulation::vInit()
 {
 	vpcIndividuals = new std::vector<CIndividual*>(I_START_POP);
-	for (int i = 0; i < I_START_POP; i++) {
-		vpcIndividuals->at(i) = new CIndividual(cOpt, cEv);
-		vpcEvaluators.push_back(new CLFLnetEvaluator());
-	}
-	for (int i = I_START_POP; i < I_POP_SIZE; i++) {
-		vpcEvaluators.push_back(new CLFLnetEvaluator());
-	}
-
-	auto sName = cEv->sGetNetName();
-
 #pragma omp parallel for
-	for (int i = 0; i < I_POP_SIZE; ++i) {
-		vpcEvaluators[i]->bConfigure(sName);
-	}		
+	for (int i = 0; i < I_START_POP; i++) {
+		vpcIndividuals->at(i) = new CIndividual(cOpt, pvpcEvaluators->at(i));
+	}
+		
 }
 
 
@@ -59,7 +60,44 @@ double CSubPopulation::dGetBestValue()
 	return vpcIndividuals->at(0)->dGetFitness();
 }
 
-int CSubPopulation::iGetParentsId() {
+int CSubPopulation::iGetParentsId2() {
+	int iPos1 = MyMath::dRand() * (cOpt->iPrevPopSize);
+	int iPos2 = MyMath::dRand() * (cOpt->iPrevPopSize);
+	if (vpcIndividuals->at(iPos1)->dGetFitness() > vpcIndividuals->at(iPos2)->dGetFitness()) {
+		return iPos1;
+	}
+	return iPos2;
+}
+
+void CSubPopulation::vOrder99()
+{
+	double dPrevFit = 0.0;
+	double dCurrentFit;
+	int iCloneCounter = 0;
+	vector<int> vMurderSpots;
+
+	for (int i = 0; i < cOpt->iPrevPopSize; i++) {
+		dCurrentFit = vpcIndividuals->at(i)->dGetFitness();
+		if (dCurrentFit != dPrevFit) {
+			dPrevFit = dCurrentFit;
+			iCloneCounter = 0;
+		}
+		else {
+			iCloneCounter++;
+			if (iCloneCounter > I_CLONE_TH) {
+				vMurderSpots.push_back(i);
+			}
+		}
+	}
+	cout << "guys to murder: " << vMurderSpots.size() << endl;
+#pragma omp parallel for
+	for (int i = 0; i < vMurderSpots.size(); i++) {
+		vpcIndividuals->at(vMurderSpots[i])->vMutate(pvpcEvaluators->at(i));
+	}
+
+}
+
+int CSubPopulation::iGetParentsId1() {
 	int iGrpId = I_PARENTS_SUBGRPS - 1;
 	int iShift = I_OPTIONS * MyMath::dRand();
 	for (int i = 0, j = 2; i < iShift; i += j, j++) {
@@ -76,29 +114,77 @@ void CSubPopulation::vCrossMutate()
 	
 	std::vector<CIndividual*>* pvpcNewPop = new std::vector<CIndividual*>();
 	while (iNewPopSize < cOpt->iCurrentPopSize) {
+		CIndividual* pcP1;
+		CIndividual* pcP2;
+		do {
+			if ((cOpt->iGetStagnation() > 0 && cOpt->iGetStagnation() % 3 == 0) || cOpt->iGetGens() % I_ALT_PARENT_CHOOSING == 0) {
+				pcP1 = vpcIndividuals->at(iGetParentsId2());
+				pcP2 = vpcIndividuals->at(iGetParentsId2());
+			}
+			else {
+				pcP1 = vpcIndividuals->at(iGetParentsId1());
+				pcP2 = vpcIndividuals->at(iGetParentsId1());
+			}
+			//maybe?
+			if (cOpt->iGetStagnation() > 0 && cOpt->iGetStagnation() % I_ALT_PARENT_CHOOSING == 0) {
+				pcP1 = pcDaBest;
+			}
+		} while (pcP1->dGetFitness() == pcP2->dGetFitness());
 		
-		
-		CIndividual* pcP1 = vpcIndividuals->at(iGetParentsId());
-		
-		CIndividual* pcP2 = vpcIndividuals->at(iGetParentsId());
-		if (MyMath::dRand() < D_CROSSOVER_CHANCE + cOpt->dCrossPenalty) {
+		if (MyMath::dRand() < iId * D_CROSS_POP_SHIFT + D_CROSSOVER_CHANCE + cOpt->dCrossPenalty) {
 			CIndividual* pcC1 = new CIndividual(cOpt);
 			CIndividual* pcC2 = new CIndividual(cOpt);
-			CIndividual::vCrossover(pcP1, pcP2, pcC1, pcC2);
-			
-			pcC1->vMutate(cEv);
-			pcC2->vMutate(cEv);
 
+
+			//CIndividual::vCrossover(pcP1, pcP2, pcC1, pcC2);
+			//
+			//pcC1->vMutate(cEv);
+			//pcC2->vMutate(cEv);
+			//pcC2->vEvaluateFitness(cEv);
+			
+
+			int iTries = 0;
+			do {
+				pcC1->vClearSol();
+				pcC2->vClearSol();
+
+				CIndividual::vCrossover(pcP1, pcP2, pcC1, pcC2);
+
+				pcC1->vMutate(cEv);
+				pcC2->vMutate(cEv);
+
+				pcC1->vEvaluateFitness(cEv);
+				pcC2->vEvaluateFitness(cEv);
+
+				if (pcC1->dGetFitness() < pcC2->dGetFitness()) {
+
+					CIndividual* temp = pcC1;
+					pcC1 = pcC2;
+					pcC2 = temp;
+				}
+				
+				iTries++;
+			} while (iTries < 3 && pcC1->dGetFitness() < pcP2->dGetFitness());
+
+			if (pcP1->dGetFitness() >= pcC2->dGetFitness()) {
+				pvpcNewPop->push_back(new CIndividual(pcP1));
+				pvpcNewPop->at(pvpcNewPop->size() - 1)->vMutate(cEv);
+				delete pcC2;
+			}
+			else {
+				pvpcNewPop->push_back((pcC2));
+			}
 			pvpcNewPop->push_back((pcC1));
-			pvpcNewPop->push_back((pcC2));
+			//pvpcNewPop->push_back((pcC2));
 		}
 		else {
-			if (MyMath::dRand() < (D_PARENT_MUTATION + cOpt->dParentPenalty)) {
-				pcP1->vMutate(cEv);
-				pcP2->vMutate(cEv);
-			}
+			
 			pvpcNewPop->push_back(new CIndividual(pcP1));
 			pvpcNewPop->push_back(new CIndividual(pcP2));
+			if (MyMath::dRand() < (iId * D_PAR_POP_SHIFT + D_PARENT_MUTATION + cOpt->dParentPenalty)) {
+				pvpcNewPop->at(pvpcNewPop->size() - 1)->vMutate(cEv);
+				pvpcNewPop->at(pvpcNewPop->size() - 2)->vMutate(cEv);
+			}
 		}
 		iNewPopSize += 2;
 	}
@@ -118,14 +204,20 @@ std::vector<CIndividual*>* CSubPopulation::pvpcGetTopGenes(int iNum)
 	return vToRet;
 }
 
-void CSubPopulation::vMigrateInto(std::vector<CIndividual*>* vGenesToMigrate)
+void CSubPopulation::vMigrateInto(std::vector<CIndividual*>* vGenesToMigrate, int iNum)
 {
-	int iLocToMigrate = MyMath::dRand() * (cOpt->iPrevPopSize / 2 - I_BEST_TO_MIGRATE - 1);
+	int iLocToMigrate = MyMath::dRand() * (cOpt->iPrevPopSize / 2 - iNum - 1);
 #pragma omp parallel for
-	for (int i = 0; i < I_BEST_TO_MIGRATE; i++) {
+	for (int i = 0; i < iNum; i++) {
 		delete vpcIndividuals->at(cOpt->iPrevPopSize / 2 + iLocToMigrate + i);
 		vpcIndividuals->at(cOpt->iPrevPopSize / 2 + iLocToMigrate + i) = new CIndividual(vGenesToMigrate->at(i));
 	}
+}
+
+void CSubPopulation::vDoChaos(int iIndivPos, CLFLnetEvaluator* pcEvToUse)
+{
+	vpcIndividuals->at(iIndivPos)->vAddChaos(pcEvToUse);
+	vpcIndividuals->at(iIndivPos)->vEvaluateFitness(pcEvToUse);
 }
 
 
@@ -139,18 +231,34 @@ CPopulation::~CPopulation()
 			delete g;
 		delete vg;
 	}
+	for (auto e : *pvpcEvaluators)
+		delete e;
+	delete pvpcEvaluators;
+
 }
 
 void CPopulation::vInit()
 {
-	int index = 0;
-	for (auto pSub : vpcSubPopulations) {
-		pSub->vInit();
+	for (int i = 0; i < I_POP_SIZE; i++) {
+		pvpcEvaluators->push_back(new CLFLnetEvaluator());
+	}
+	auto sName = pcEv->sGetNetName();
+
+#pragma omp parallel for
+	for (int i = 0; i < I_POP_SIZE; ++i) {
+		pvpcEvaluators->at(i)->bConfigure(sName);
+	}
+
+
+	for (int index = 0; index < I_SUB_POPS; index++) {
+		vpcSubPopulations[index]->vInit();
 		vpcBestGenes.push_back(new vector<CIndividual*>());
 		for (int j = 0; j < I_BEST_TO_MIGRATE; j++) {
 			vpcBestGenes.at(index)->push_back(nullptr);
 		}
-		index++;
+	}
+	for (int index = I_SUB_POPS; index < I_SUB_POPS + I_HELPERS; index++) {
+		vpcSubPopulations[index]->vInit();
 	}
 }
 
@@ -196,23 +304,25 @@ void CPopulation::vEvalSortIndividuals()
 
 void CPopulation::vEvalSortAll()
 {
-	vpcSubPopulations[I_SUB_POPS]->vEvalSortIndividuals();
-	vEvalSortIndividuals();
+	for (int i = 0; i < I_SUB_POPS + I_HELPERS; i++) {
+		vpcSubPopulations[i]->vEvalSortIndividuals();
+	}
+	
 }
 
 void CPopulation::vCrossMutate()
 {
 #pragma omp parallel for
-	for (int i = 0; i <= I_SUB_POPS; i++) {
+	for (int i = 0; i < I_SUB_POPS + I_HELPERS; i++) {
 		vpcSubPopulations[i]->vCrossMutate();
 	}
 }
 
 void CPopulation::vExchangeBestGenes()
 {
-
-	for (int i = 0; i < I_SUB_POPS; i++) {
+	cout << "-----MIGRATION-----\n";
 #pragma omp parallel for
+	for (int i = 0; i < I_SUB_POPS; i++) {
 		for (int j = 0; j < I_BEST_TO_MIGRATE; j++) {
 			delete vpcBestGenes[i]->at(j);
 		}
@@ -232,19 +342,60 @@ void CPopulation::vExchangeBestGenes()
 			int iIndividual = MyMath::dRand() * I_BEST_TO_MIGRATE;
 			pvpcGenesToMigrate->at(j) = (vpcBestGenes[iIndex]->at(iIndividual));	
 		}
-		vpcSubPopulations[i]->vMigrateInto(pvpcGenesToMigrate);
+		vpcSubPopulations[i]->vMigrateInto(pvpcGenesToMigrate, I_BEST_TO_MIGRATE);
 		delete pvpcGenesToMigrate;
 	}
 }
 
 void CPopulation::vGenBestFromHelper()
 {
-	vector<CIndividual*>* pvpcGenesToMigrate = vpcSubPopulations[I_SUB_POPS]->pvpcGetTopGenes(I_BEST_TO_MIGRATE);
-	for (int i = 0; i < I_SUB_POPS; i++) {
-		vpcSubPopulations[i]->vMigrateInto(pvpcGenesToMigrate);
-	}
+	cout << "-----BEST FROM HELPER-----\n";
+	int iHelper = MyMath::dRand() * I_HELPERS;
+	vector<CIndividual*>* pvpcGenesToMigrate = vpcSubPopulations[I_SUB_POPS + iHelper]->pvpcGetTopGenes(I_BEST_TO_MIGRATE/2);
+	int iSubPop = MyMath::dRand() * I_SUB_POPS;
+	vpcSubPopulations[iSubPop]->vMigrateInto(pvpcGenesToMigrate, I_BEST_TO_MIGRATE / 2);
+	
 	for (auto pcInd : *pvpcGenesToMigrate) {
 		delete pcInd;
 	}
 	delete pvpcGenesToMigrate;
+}
+
+void CPopulation::vUnleashChaos()
+{
+	cout << "----------CHAOS------------\n";
+	vector<int>vPos = vGetRandomRange();
+
+#pragma omp parallel for
+	for (int i = 0; i < I_CHAOS_COUNT; i++) {
+		int iPop = MyMath::dRand()* (I_SUB_POPS + I_HELPERS);
+		vpcSubPopulations[iPop]->vDoChaos(vPos[i], pvpcEvaluators->at(i));
+	}
+}
+
+vector<int> CPopulation::vGetRandomRange()
+{
+	int iMax = pcOpt->iPrevPopSize;
+	vector<int> list(iMax);
+	for (int i = 0; i < iMax; i++) {
+		list[i] = i;
+	}
+	vector<int> res(I_CHAOS_COUNT);
+	for (int i = 0; i < I_CHAOS_COUNT; i++) {
+		int j = i + MyMath::dRand() * (iMax - i);
+		int temp = list[i];
+		list[i] = list[j];
+		list[j] = temp;
+
+		res[i] = list[i];
+	}
+	return res;
+}
+
+void CPopulation::vExterminateClones()
+{
+	cout << "-----------ORDER 99\n";
+	for (int i = 0; i < I_SUB_POPS + I_HELPERS; i++) {
+		vpcSubPopulations[i]->vOrder99();
+	}
 }
